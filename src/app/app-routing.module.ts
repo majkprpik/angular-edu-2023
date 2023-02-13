667c18c80fed29f5fcc892a45235a29f7bde59e1
Teo <tkupcino@gmail.com>
2023-02-13 18:15:56 +0100

    Teo module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,13 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/test/test.module').then((m) => m.TestModule),
   },
+  {
+    path: 'my-module15',
+    loadChildren: () =>
+      import('./modules/my-module15/my-module15.module').then(
+        (m) => m.MyModule15Module
+      ),
+  },
 ];
 
 @NgModule({
