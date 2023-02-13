ed6b1a17b85944d8ac861c314167097a1ab48625
majkprpik <majkprpik@users.noreply.github.com>
2023-02-13 17:28:41 +0100

    test module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    loadChildren: () =>
+      import('./modules/test/test.module').then((m) => m.TestModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
