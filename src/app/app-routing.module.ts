69f02a3e59c4efbeb82726430e29e5ff8feae269
MislavCacic <mcacic99@gmail.com>
2023-02-13 19:07:40 +0100

    popravci

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,3 @@
-
-import { KarlaModule } from './modules/karla/karla.module';
-import { TestModule } from './modules/test/test.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -9,18 +6,12 @@ const routes: Routes = [
     path: 'test',
     loadChildren: () =>
       import('./modules/test/test.module').then((m) => m.TestModule),
-    //import('src/app/modules/dino/dino.module').then((n)=>n.DinoModule)
   },
   {
     path: 'Dino',
     loadChildren: () =>
       import('./modules/dino/dino.module').then((m) => m.DinoModule),
   },
-  // {
-  //   path: 'tihomir',
-  //   loadChildren: () =>
-  //     import('./modules/tihomir/tihomir.module').then((m) => m.TihomirModule),
-  // },
   {
     path: 'filip',
     loadChildren: () =>
@@ -43,21 +34,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/ivan/ivan.module').then((m) => m.IvanModule),
   },
-  {
-    path: 'mislav-cacic',
-    loadChildren: () =>
-      import('./modules/mislav-cacic/mislav-cacic.module').then(
-        (m) => m.MislavCacicModule
-      ),
-  },
-  {
-    path: 'filip',
-    loadChildren: () =>
-      import('./modules/filip/filip.module').then((m) => m.FilipModule),
-    path: 'karla',
-    loadChildren: () =>
-      import('./modules/karla/karla.module').then((m) => m.KarlaModule),
-  }
 ];
 
 @NgModule({
