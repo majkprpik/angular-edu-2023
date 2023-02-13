98755b569c96cdac418f516e4f6e81c0c15a2fed
karlafehir <fehirkarla@gmail.com>
2023-02-13 17:57:02 +0100

    added module

---
+++ src/app/modules/karla/karla.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KarlaRoutingModule } from './karla-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    KarlaRoutingModule
+  ]
+})
+export class KarlaModule { }
