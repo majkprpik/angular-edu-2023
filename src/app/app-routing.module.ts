03a3dea3974dd9f5cd27947fafeeea3c12994197
filip-tod <filip.tod95@gmail.com>
2023-02-13 18:10:32 +0100

    filip module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FilipModule } from './modules/filip/filip.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -32,6 +33,11 @@ const routes: Routes = [
         (m) => m.MislavCacicModule
       ),
   },
+  {
+    path: 'filip',
+    loadChildren: () =>
+      import('./modules/filip/filip.module').then((m) => m.FilipModule),
+  }
 ];
 
 @NgModule({
---
+++ src/app/modules/filip/filip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filip',
+  templateUrl: './filip.component.html',
+  styleUrls: ['./filip.component.scss']
+})
+export class FilipComponent {
+
+}
