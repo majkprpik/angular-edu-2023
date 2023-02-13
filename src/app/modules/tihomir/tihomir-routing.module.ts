56acd51c960216aad6dd2dac119564703be7a021
TihoAska <tihomirascic18@gmail.com>
2023-02-13 18:23:48 +0100

    dodao svoj link

--- src/app/modules/tihomir/tihomir-routing.module.ts
+++ src/app/modules/tihomir/tihomir-routing.module.ts
@@ -1,8 +1,12 @@
+import { TihomirComponent } from './tihomir.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path : '',
+    component : TihomirComponent
+  }
 ];
 
 @NgModule({
