bab723860275ad26ef037313bc1a76aff6b82f28
DinoKnezevic1 <knezevic7p@gmail.com>
2023-02-13 18:31:59 +0100

    conflict resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,16 +10,15 @@ const routes: Routes = [
       //import('src/app/modules/dino/dino.module').then((n)=>n.DinoModule)
   },
   {
-<<<<<<< HEAD
     path:'Dino',
     loadChildren:()=>
     import('./modules/dino/dino.module').then((m)=>m.DinoModule)
-=======
+  },
+  {
     path: 'tihomir',
     loadChildren: () =>
       import('./modules/tihomir/tihomir.module').then((m) => m.TihomirModule),
->>>>>>> 56acd51c960216aad6dd2dac119564703be7a021
-  }
+  },
 ];
 
 @NgModule({
