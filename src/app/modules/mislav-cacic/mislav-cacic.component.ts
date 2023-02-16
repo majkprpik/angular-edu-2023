1e5346ab7d628b7f5ce668843797fc4602c41422
MislavCacic <mcacic99@gmail.com>
2023-02-16 21:49:56 +0100

    loggin form

--- src/app/modules/mislav-cacic/mislav-cacic.component.ts
+++ src/app/modules/mislav-cacic/mislav-cacic.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mislav-cacic',
   templateUrl: './mislav-cacic.component.html',
-  styleUrls: ['./mislav-cacic.component.scss']
+  styleUrls: ['./mislav-cacic.component.scss'],
 })
 export class MislavCacicComponent {
+  person = {
+    username: '',
+    password: '',
+  };
 
+  login() {
+    console.log(this.person);
+  }
 }
