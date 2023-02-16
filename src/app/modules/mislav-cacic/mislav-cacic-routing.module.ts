530a9495998405ae1b7dbeacd7755fed580a7212
MislavCacic <mcacic99@gmail.com>
2023-02-16 21:54:07 +0100

    loggin form

--- src/app/modules/mislav-cacic/mislav-cacic-routing.module.ts
+++ src/app/modules/mislav-cacic/mislav-cacic-routing.module.ts
@@ -1,16 +1,16 @@
-import { MislavCacicComponent } from './mislav-cacic.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MislavCacicComponent } from './mislav-cacic.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MislavCacicComponent,
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MislavCacicRoutingModule { }
+export class MislavCacicRoutingModule {}
