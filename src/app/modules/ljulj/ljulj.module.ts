c77d1495ba5488a40dfe1d285429e57b9a9aecf3
nolsos <fljulj@gmail.com>
2023-02-17 17:43:22 +0100

    ok

--- src/app/modules/ljulj/ljulj.module.ts
+++ src/app/modules/ljulj/ljulj.module.ts
@@ -5,14 +5,8 @@ import { CommonModule } from '@angular/common';
 import { LjuljRoutingModule } from './ljulj-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LjuljRoutingModule, FormsModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, LjuljRoutingModule, FormsModule],
 })
-export class LjuljModule { } 
+export class LjuljModule {}
