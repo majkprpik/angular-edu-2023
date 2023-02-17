430f1a6858d430a83715cb2e989fe081564d038c
DinoKnezevic1 <knezevic7p@gmail.com>
2023-02-17 16:54:24 +0100

    branch fix

--- src/app/modules/dino/dino.module.ts
+++ src/app/modules/dino/dino.module.ts
@@ -1,17 +1,21 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DinoRoutingModule } from './dino-routing.module';
 import { DinoComponent } from './dino.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    DinoComponent
+    DinoComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    DinoRoutingModule
+    DinoRoutingModule,
+    FormsModule
   ]
 })
 export class DinoModule { }
