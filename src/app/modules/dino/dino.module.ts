7cb7b3981144fa2fdc8d5dcb4eaeab0b39cd1cb5
DinoKnezevic1 <knezevic7p@gmail.com>
2023-02-17 16:11:27 +0100

    login update

--- src/app/modules/dino/dino.module.ts
+++ src/app/modules/dino/dino.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DinoRoutingModule } from './dino-routing.module';
 import { DinoComponent } from './dino.component';
-import { LoginComponent } from './components/login/login.component';
+//import { LoginComponent } from './components/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     DinoComponent,
     LoginComponent,
+    //LoginComponent,
   ],
   imports: [
     CommonModule,
