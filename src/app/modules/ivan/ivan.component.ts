97d1a99c4df8bd2b35b895d53bd8d84c78da0feb
DinoKnezevic1 <knezevic7p@gmail.com>
2023-02-17 15:56:55 +0100

    upload fix

--- src/app/modules/ivan/ivan.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ivan',
-  templateUrl: './ivan.component.html',
-  styleUrls: ['./ivan.component.scss'],
-})
-export class IvanComponent {}
