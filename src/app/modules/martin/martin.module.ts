1276b571d9de87f05c1fdb7ca7abab065813161a
martinm18 <martin.mata69@gmail.com>
2023-02-17 16:08:33 +0100

    login forma

--- src/app/modules/martin/martin.module.ts
+++ src/app/modules/martin/martin.module.ts
@@ -1,17 +1,22 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MartinRoutingModule } from './martin-routing.module';
 import { MartinComponent } from './martin.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 
 @NgModule({
   declarations: [
-    MartinComponent
+    MartinComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    MartinRoutingModule
+    MartinRoutingModule,
+    FormsModule
   ]
 })
 export class MartinModule { }
