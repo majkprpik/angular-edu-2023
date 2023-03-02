bfaf3c07b6ed0766507935486eebc015be67f5ca
martinm18 <martin.mata69@gmail.com>
2023-03-02 13:57:37 +0100

    hhhh

--- src/app/modules/martin/martin-routing.module.ts
+++ src/app/modules/martin/martin-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            component: DashboardComponent,
+            component: ListComponent,
           },
           {
             path: 'grid',
