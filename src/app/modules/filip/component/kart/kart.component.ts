1851b420396247bc96a81f55cecbe8fee767c089
filip-tod <filip.tod95@gmail.com>
2023-03-23 12:31:37 +0100

    kart komponenta prima podatke o kupovini

--- src/app/modules/filip/component/kart/kart.component.ts
+++ src/app/modules/filip/component/kart/kart.component.ts
@@ -1,29 +1,20 @@
-import { Product } from './../../models/product';
-import { cartItem } from './../../models/cart-item';
+import { cart } from './../../models/cart';
 import { KartService } from './../../service/kart.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-kart',
   templateUrl: './kart.component.html',
-  styleUrls: ['./kart.component.scss']
+  styleUrls: ['./kart.component.scss'],
 })
 export class KartComponent {
- 
-constructor(private cartService: KartService){
+itemsSelected: cart[]= []
 
-}
+  constructor(private cartService: KartService) {
+    this.cartService.$cart.subscribe((data) => {
+      this.itemsSelected.push(data) ;
+    });
+  }
 
-showItems(cartItem: cartItem){
-  
-}
-
-// constructor(private kartService:KartService, private cart:cart){}
-// kosaricaProduct: Product[]=[]
-
-//  getProducts(Product:Product) {
-//   this.cart.addItem(this.kosaricaProduct)
-//   }
-
-
+  showItems() {}
 }
--- src/app/modules/filip/component/product/product.component.ts
+++ src/app/modules/filip/component/product/product.component.ts
@@ -6,31 +6,27 @@ import { Product } from '../../models/product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-sviProducti: Product[]=[];
+  sviProducti: Product[] = [];
 
+  constructor(
+    private productService: ProductService,
+    private cartService: KartService
+  ) {
+    this.productService.$products.subscribe((data) => {
+      this.sviProducti = data;
+    });
+    this.productService.$sortAscending.subscribe((data) => {
+      this.sortAscending = data;
+    });
+  }
+  //servis za pipe
+  addToCart(product: Product) {
+    this.cartService.addItem(product);
+  }
+  products: Product[] = [];
 
-constructor(private productService: ProductService, private cartService: KartService){
-  this.productService.$products.subscribe((data)=>{
-    this.sviProducti=data
-  })
-  this.productService.$sortAscending.subscribe((data)=>{
-    this.sortAscending=data
-  })
-}
-//servis za pipe
- addToCart(product: Product){
-this.cartService.addItem(product)
-
- }
-products: Product[] = [];
-
-sortAscending = true;
-
-
-
-
-// Product u "model-products" nije importan
+  sortAscending = true;
 }
