6ff581f21af2a9fd86fb3645a7a4837730263bc4
DinoKnezevic1 <knezevic7p@gmail.com>
2023-03-23 17:58:29 +0100

    stjepan merge eerror

--- src/app/modules/stjepan/stjepan.module.ts
+++ src/app/modules/stjepan/stjepan.module.ts
@@ -5,19 +5,9 @@ import { CommonModule } from '@angular/common';
 
 import { StjepanRoutingModule } from './stjepan-routing.module';
 import { StjepanComponent } from './stjepan.component';
-<<<<<<< HEAD
 
 @NgModule({
   declarations: [StjepanComponent],
   imports: [CommonModule, StjepanRoutingModule, FormsModule],
-=======
-import { AutenService } from './services/auth.service';
-import { LoginComponent } from './login/login/login.component';
-
-@NgModule({
-  declarations: [StjepanComponent, LoginComponent],
-  imports: [CommonModule, StjepanRoutingModule, FormsModule, HttpClientModule],
-  providers: [AutenService],
->>>>>>> main
 })
 export class StjepanModule {}
